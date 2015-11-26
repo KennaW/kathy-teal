25e941eef48ba6de45bd90f714e45cb59e55e54f
KennaW <KennaWarsinske@gmail.com>
2015-11-25 23:40:45 -0800

    add span to php

---
+++ category-list.php
@@ -0,0 +1,1 @@
+<li><a href="[category_url]">[category_thumbnail]<br /><span>[category_name]</span></a></li>
---
+++ item-list.php
@@ -0,0 +1,1 @@
+<li><a href="[item_url]">[item_photo]<br /><span>[item_name]</span></a></li>
