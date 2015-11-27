0aae778ee1557139a7a2c0b8e8bc0e9227fd4ea0
KennaW <KennaWarsinske@gmail.com>
2015-11-26 21:45:05 -0800

    opacity on images and move up caption

--- category-list.php
+++ category-list.php
@@ -1,1 +1,1 @@
-<li><a href="[category_url]">[category_thumbnail]<br /><span class="item__caption">[category_name]</span></a></li>
+<li><a class="list__item" href="[category_url]">[category_thumbnail]<br /><span class="item__caption">[category_name]</span></a></li>
