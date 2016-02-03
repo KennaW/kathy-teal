c16c611893e4c4caa1d077c0ba450fdc3693632e
KennaW <KennaWarsinske@gmail.com>
2016-02-02 20:13:18 -0800

    additional wishlist fields

--- wishlist-extra-fields.php
+++ wishlist-extra-fields.php
@@ -1,4 +1,11 @@
 <p>
-  <label for="venue">venue</label>
+  <label for="venue">Location/Venue</label>
   <input id="venue" name="venue" type="text" value="">
+
+  <label for="pickup">Will you require pickup?</label>
+  <input id="pickup" name="pickup" type="radio" value="yes"> Yes
+  <input id="pickup" name="pickup" type="radio" value="no"> No
+
+  <label for="othernotes">Additional notes</label>
+  <textarea cols="40" rows="5" name="othernotes"></textarea>
 </p>
